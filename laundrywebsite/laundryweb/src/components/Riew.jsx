c6fb08d31ca4f37b9cc425b159b27d775340dda1
cantikawijaya <cantikawjy@gmail.com>
2025-01-03 01:10:10 +0800

    Perbaikan tampilan footer

--- laundrywebsite/laundryweb/src/components/Riew.jsx
+++ laundrywebsite/laundryweb/src/components/Riew.jsx
@@ -16,137 +16,141 @@ const Riew = () => {
       }) 
     }, [])
 
-  return (
-    <div>
-      <h1 data-aos="fade-up"
-        className=" justify-center text-center font-bold text-[300px]"
-        style={{
-          color: "black",
-          fontSize: "24pt",
-          fontWeight: "bold",
-          marginTop: "200px",
-          marginBottom: "100px",
-        }}
-      >
-        Riew Pelanggan Kami
-      </h1>
-      <div data-aos="fade-up" className="d-flex justify-content-around">
-        {/* Review 1 */}
-        <Card data-aos="fade-up"
+    return (
+      <div>
+        <h1
+          data-aos="fade-up"
+          className="text-center font-bold"
           style={{
-            backgroundColor: "#D9EAF4",
-            width: "18rem",
-            boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
-            borderRadius: "20px",
-            margin: "0 10px",
+            color: "black",
+            fontSize: "24pt",
+            fontWeight: "bold",
+            marginTop: "200px",
+            marginBottom: "100px",
           }}
         >
-          <div 
+          Review Pelanggan Kami
+        </h1>
+        <div data-aos="fade-up" className="d-flex justify-content-around">
+          {/* Review 1 */}
+          <Card
+            data-aos="fade-up"
             style={{
-              display: "flex",
-              justifyContent: "center",
-              marginTop: "20px",
+              backgroundColor: "#D9EAF4",
+              width: "18rem",
+              boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+              borderRadius: "20px",
+              margin: "0 10px",
             }}
           >
-            <Card.Img
-              variant="top"
-              src={Larasati}
-              alt="larasai"
+            <div
               style={{
-                width: "200px",
-                height: "200px",
-                objectFit: "cover",
-                borderRadius: "20px",
+                display: "flex",
+                justifyContent: "center",
+                marginTop: "20px",
               }}
-            />
-          </div>
-          <Card.Body style={{ textAlign: "center" }}>
-            <Card.Title style={{ fontWeight: "bold" }}>Larasati</Card.Title>
-            <Card.Text>
-              Aplikasi laundry ini bener-bener nyelamatkan hidupku, tinggal
-              pilih layanan, jemput, dan hasilnya bersih banget!
-            </Card.Text>
-          </Card.Body>
-        </Card>
-
-        {/* Review 2 */}
-        <Card data-aos="fade-up"
-          style={{
-            backgroundColor: "#D9EAF4",
-            width: "18rem",
-            boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
-            borderRadius: "20px",
-            margin: "0 10px",
-          }}
-        >
-          <div
+            >
+              <Card.Img
+                variant="top"
+                src={Larasati}
+                alt="larasati"
+                style={{
+                  width: "200px",
+                  height: "200px",
+                  objectFit: "cover",
+                  borderRadius: "20px",
+                }}
+              />
+            </div>
+            <Card.Body style={{ textAlign: "center" }}>
+              <Card.Title style={{ fontWeight: "bold", fontSize: "16pt" }}>Larasati</Card.Title>
+              <Card.Text style={{ fontSize: "14pt" }}>
+                Aplikasi laundry ini bener-bener nyelamatkan hidupku, tinggal pilih
+                layanan, jemput, dan hasilnya bersih banget!
+              </Card.Text>
+            </Card.Body>
+          </Card>
+    
+          {/* Review 2 */}
+          <Card
+            data-aos="fade-up"
             style={{
-              display: "flex",
-              justifyContent: "center",
-              marginTop: "20px",
+              backgroundColor: "#D9EAF4",
+              width: "18rem",
+              boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+              borderRadius: "20px",
+              margin: "0 10px",
             }}
           >
-            <Card.Img
-              variant="top"
-              src={Rangga}
-              alt="rangga"
+            <div
               style={{
-                width: "200px",
-                height: "200px",
-                objectFit: "cover",
-                borderRadius: "20px",
+                display: "flex",
+                justifyContent: "center",
+                marginTop: "20px",
               }}
-            />
-          </div>
-          <Card.Body style={{ textAlign: "center" }}>
-            <Card.Title style={{ fontWeight: "bold" }}>Rangga</Card.Title>
-            <Card.Text>
-              Puas banget pake aplikasi laundry ini, cepet, simpel, dan harganya
-              juga oke. No more ribet cari tempat cuci!
-            </Card.Text>
-          </Card.Body>
-        </Card>
-
-        {/* Review 3 */}
-        <Card data-aos="fade-up"
-          style={{
-            backgroundColor: "#D9EAF4",
-            width: "18rem",
-            boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
-            borderRadius: "20px",
-            margin: "0 10px",
-          }}
-        >
-          <div
+            >
+              <Card.Img
+                variant="top"
+                src={Rangga}
+                alt="rangga"
+                style={{
+                  width: "200px",
+                  height: "200px",
+                  objectFit: "cover",
+                  borderRadius: "20px",
+                }}
+              />
+            </div>
+            <Card.Body style={{ textAlign: "center" }}>
+              <Card.Title style={{ fontWeight: "bold", fontSize: "16pt" }}>Rangga</Card.Title>
+              <Card.Text style={{ fontSize: "14pt" }}>
+                Puas banget pake aplikasi laundry ini, cepet, simpel, dan harganya
+                juga oke. No more ribet cari tempat cuci!
+              </Card.Text>
+            </Card.Body>
+          </Card>
+    
+          {/* Review 3 */}
+          <Card
+            data-aos="fade-up"
             style={{
-              display: "flex",
-              justifyContent: "center",
-              marginTop: "20px",
+              backgroundColor: "#D9EAF4",
+              width: "18rem",
+              boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+              borderRadius: "20px",
+              margin: "0 10px",
             }}
           >
-            <Card.Img
-              variant="top"
-              src={Sarah}
-              alt="sarah"
+            <div
               style={{
-                width: "200px",
-                height: "200px",
-                objectFit: "cover",
-                borderRadius: "20px",
+                display: "flex",
+                justifyContent: "center",
+                marginTop: "20px",
               }}
-            />
-          </div>
-          <Card.Body style={{ textAlign: "center" }}>
-            <Card.Title style={{ fontWeight: "bold" }}>Sarah</Card.Title>
-            <Card.Text>
-              Layanan pelanggan laundry ramah banget, selalu siap bantu. Pakaian
-              ku juga selalu wangi dan rapi setiap kali diantar kembali.
-            </Card.Text>
-          </Card.Body>
-        </Card>
+            >
+              <Card.Img
+                variant="top"
+                src={Sarah}
+                alt="sarah"
+                style={{
+                  width: "200px",
+                  height: "200px",
+                  objectFit: "cover",
+                  borderRadius: "20px",
+                }}
+              />
+            </div>
+            <Card.Body style={{ textAlign: "center" }}>
+              <Card.Title style={{ fontWeight: "bold", fontSize: "16pt" }}>Sarah</Card.Title>
+              <Card.Text style={{ fontSize: "14pt" }}>
+                Layanan pelanggan laundry ramah banget, selalu siap bantu. Pakaian
+                ku juga selalu wangi dan rapi setiap kali diantar kembali.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
       </div>
-    </div>
-  );
+    );        
 };
 
 export default Riew;
